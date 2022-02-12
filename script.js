961b68e313a5f25596367a340289fdff3ebc2805
Sione <sione.vuki@hgs.hiddengeniusproject.org>
2022-02-12 19:12:43 +0000

    Activity 2

---
+++ script.js
@@ -0,0 +1,5 @@
+var name = "Sione";
+var names = " Omari and Peyton"
+var message= "We needa get this bread!!!";
+
+console.log("My name is "+ name +". I am on a team with "+ names +". I want my bros to know that "+ message +".");
